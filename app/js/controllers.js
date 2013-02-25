41f4e9bde5fb6e8300492b5882c62229122df36d
Bret Little <blittle@simplifile.com>
2013-02-25 14:08:12 -0700

    Add a basic image controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,7 +6,9 @@
 function MyCtrl1() {}
 MyCtrl1.$inject = [];
 
-
-function MyCtrl2() {
-}
+function MyCtrl2() {}
 MyCtrl2.$inject = [];
+
+function ImagesController($scope, Image) {
+    $scope.image = Image.query() 
+}
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,14 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+
+angular.module('apod.services', ['ngResource']).
+    value('version', '0.1').
+    factory('Image', function($resource, $http) {
+        delete $http.defaults.headers.common['X-Requested-With'];
+
+        return $resource('http://209.180.92.243\\:8080/apod/images/:days', {}, {
+            query: {method:'GET', params: {days: '1'}, isArray: true}
+        });
+    });
+
